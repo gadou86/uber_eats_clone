6d7804d9e6ec6c9511dc5ef13f09f2a0d73632ef
Gad Dayan <70294896+gadou86@users.noreply.github.com>
2021-11-14 12:33:37 -0500

    Added assets and image and started categorie view

--- screens/Home.js
+++ screens/Home.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { View, Text, SafeAreaView } from 'react-native'
 import HeaderTabs from '../components/HeaderTabs';
 import SearchBar from '../components/SearchBar';
+import Categories from '../components/Categories';
+
+
 
 
 export default function Home() {
@@ -10,7 +13,8 @@ export default function Home() {
       <View style={{backgroundColor: "white", padding: 15}}>
         <HeaderTabs />    
         <SearchBar />
-      </View>    
+      </View> 
+      <Categories />   
     </SafeAreaView>
   );
 }
